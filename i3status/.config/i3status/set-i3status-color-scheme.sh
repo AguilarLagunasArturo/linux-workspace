6119cca10089ba594c869610b12c42c04f6f3ab7
AguilarLagunasArturo <aguilar.lagunas.arturo@gmail.com>
2021-09-10 10:08:36 -0500

    Update

--- i3status/.config/i3status/set-i3status-color-scheme.sh
+++ i3status/.config/i3status/set-i3status-color-scheme.sh
@@ -4,4 +4,11 @@
 # Date:			Fri 10 Sep 2021 01:46:57 AM CDT
 # Description:	Set xresources color scheme to i3status
 
-cat config | grep -i '#[a-fA-F0-9]\{6\}'
+config=$HOME/.config/i3status/config
+c10=$($HOME/.homemade/bin/get-xresources-color color10)
+c13=$($HOME/.homemade/bin/get-xresources-color color13)
+c9=$($HOME/.homemade/bin/get-xresources-color color9)
+
+sed --follow-symlinks -i 's/^.*color_good.*#[a-fA-F0-9]\{6\}"/        color_good = "'$c10'"/' $config
+sed --follow-symlinks -i 's/^.*color_degraded.*#[a-fA-F0-9]\{6\}"/        color_degraded = "'$c13'"/' $config
+sed --follow-symlinks -i 's/^.*color_bad.*#[a-fA-F0-9]\{6\}"/        color_bad = "'$c9'"/' $config
